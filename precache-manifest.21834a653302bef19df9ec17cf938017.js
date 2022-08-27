a6859f300ee61cb8e04b2d801cb74d4a08f77514
sahil <guptasahil198@gmail.com>
2022-08-27 16:39:05 +0530

    Updates

---
+++ precache-manifest.21834a653302bef19df9ec17cf938017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4ce5fdb8b1022fd5191",
+    "url": "/TextUtils/static/css/main.8cf4da91.chunk.css"
+  },
+  {
+    "revision": "e4ce5fdb8b1022fd5191",
+    "url": "/TextUtils/static/js/main.e4ce5fdb.chunk.js"
+  },
+  {
+    "revision": "b4a19630c54a44cacb4b",
+    "url": "/TextUtils/static/js/1.b4a19630.chunk.js"
+  },
+  {
+    "revision": "1be3877bbfc18b57cc64",
+    "url": "/TextUtils/static/js/2.1be3877b.chunk.js"
+  },
+  {
+    "revision": "d7b5cb5ffa7008e79bb6",
+    "url": "/TextUtils/static/js/runtime~main.d7b5cb5f.js"
+  },
+  {
+    "revision": "b5e196c08a94903cfa88d9d90c95a181",
+    "url": "/TextUtils/index.html"
+  }
+];
